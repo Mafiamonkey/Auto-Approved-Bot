868f19ca140054d975f9a40c087705f984cdf15d
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+200920082007@users.noreply.github.com>
2023-03-15 18:12:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,9 +20,9 @@ APPROVED_WELCOME = environ.get("APPROVED_WELCOME", "on").lower() == "on"
 
 @pr0fess0r_99.on_chat_join_request()
 async def autoapprove(client: pr0fess0r_99, message: ChatJoinRequest):
-    chat=message.chat # Chat
-    user=message.from_user # User
-    print(f"{user.first_name} Joined 🤝") # Logs
+    chat = message.chat  # Chat
+    user = message.from_user  # User
+    print(f"{user.first_name} Joined 🤝")  # Logs
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
     if APPROVED_WELCOME:
         await client.send_message(chat_id=chat.id, text=APPROVED_WELCOME_TEXT.format(mention=user.mention, title=chat.title))
@@ -30,6 +30,8 @@ async def autoapprove(client: pr0fess0r_99, message: ChatJoinRequest):
         log_chat = await client.get_chat(chat_id=CHAT_ID)
         log_text = f"{user.first_name} ({user.mention}) has been auto-approved to {chat.title} ({chat.username})"
         await client.send_message(chat_id=log_chat.id, text=log_text)
+    # Send a welcome message to the user
+    await client.send_message(chat_id=user.id, text="Hi, welcome to the group!")
 
 from pymongo import MongoClient
 
