1da3c38a1d1741462b0509fa6a60d125f3b6f88c
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+200920082007@users.noreply.github.com>
2023-03-15 13:29:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,3 @@
-# MIT License
-
-# Copyright (c) 2022 Muhammed
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-# Telegram Link : https://telegram.dog/Mo_Tech_Group
-# Repo Link : https://github.com/PR0FESS0R-99/Auto-Approved-Bot
-# License Link : https://github.com/PR0FESS0R-99/Auto-Approved-Bot/blob/Auto-Approved-Bot/LICENSE
-
 from os import environ
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, User, ChatJoinRequest
@@ -79,9 +53,13 @@ def start_command(client, message):
         }
         collection.insert_one(user)
     # send welcome message
-    client.send_message(message.chat.id, '<b>Welcome to the bot, __Add This Bot To Your Channels or Groups To Accept Join Requests Automatically__\n\n 😊By Team @CineMaVilla</b>')
+    text = '<b>Welcome to the bot, __Add This Bot To Your Channels or Groups To Accept Join Requests Automatically__\n\n 😊By Team @CineMaVilla</b>'
+    keyboard = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Updates Channel", url="https://t.me/Cinema_Villa")],
+        [InlineKeyboardButton("Support Group", url="https://t.me/Cinema_Villa_Group")]
+    ])
+    client.send_message(message.chat.id, text, reply_markup=keyboard, parse_mode='html')
 
-# define broadcast command to send message to all users
 @pr0fess0r_99.on_message(filters.command('broadcast'))
 def broadcast_command(client, message):
     msg = ' '.join(message.command[1:])
@@ -94,7 +72,7 @@ def broadcast_command(client, message):
 @pr0fess0r_99.on_message(filters.command('users'))
 def users_command(client, message):
     users = collection.find()
-    if users.count() == 0:
+    if users.count_documents({}) == 0:
         client.send_message(message.chat.id, 'No users found.')
         return
     user_list = []
